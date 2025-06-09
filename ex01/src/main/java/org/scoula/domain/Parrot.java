78bc4172aa1c90dc5eaf7e542f760e40cd902ec6
Whatdoyumin <lym4963@naver.com>
2025-06-09 10:22:06 +0900

    [1일차] PostConstruct 사용하여 인스턴스 생성 후 관리

--- ex01/src/main/java/org/scoula/domain/Parrot.java
+++ ex01/src/main/java/org/scoula/domain/Parrot.java
@@ -2,9 +2,16 @@ package org.scoula.domain;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class Parrot {       // 디폴트 컴포넌트의 name : 클래스명의 camelCase - parrot
     private String name;
+
+    @PostConstruct
+    public void init() {
+        this.name = "Kiki";
+    }
 
     public String getName() {
         return name;
