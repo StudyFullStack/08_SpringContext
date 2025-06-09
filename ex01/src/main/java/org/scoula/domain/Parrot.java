e2b48dba892722cce621413774204c17519de498
Whatdoyumin <lym4963@naver.com>
2025-06-09 10:18:21 +0900

    [1일차] 스테레오타입 애너테이션으로 스프링 컨텍스트에 빈 추가

--- ex01/src/main/java/org/scoula/domain/Parrot.java
+++ ex01/src/main/java/org/scoula/domain/Parrot.java
@@ -1,6 +1,9 @@
 package org.scoula.domain;
 
-public class Parrot {
+import org.springframework.stereotype.Component;
+
+@Component
+public class Parrot {       // 디폴트 컴포넌트의 name : 클래스명의 camelCase - parrot
     private String name;
 
     public String getName() {
