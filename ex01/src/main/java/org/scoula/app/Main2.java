16cc13a785a64c245470d608074c09ced2fa1872
Whatdoyumin <lym4963@naver.com>
2025-06-09 10:15:32 +0900

    [1일차] 빈의 이름 등록 후 스프링 컨텍스트에 추출

--- ex01/src/main/java/org/scoula/app/Main2.java
+++ ex01/src/main/java/org/scoula/app/Main2.java
@@ -8,10 +8,10 @@ public class Main2 {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig2.class);
 
-        Parrot p = context.getBean(Parrot.class);      // 예외 발생!!
-        System.out.println(p.getName());
+//        Parrot p = context.getBean(Parrot.class);      // 예외 발생!!
+//        System.out.println(p.getName());
 
-        Parrot p = context.getBean("miki", Parrot.class);
-        System.out.println(p.getName());
+        Parrot p2 = context.getBean("miki", Parrot.class);
+        System.out.println(p2.getName());
     }
 }
